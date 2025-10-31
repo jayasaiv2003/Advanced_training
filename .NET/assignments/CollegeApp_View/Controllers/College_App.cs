c51a0200289fac892cc170ab145b4629fe5fb598
jayasaiv2003 <Veeramallu.rao@esyasoft.com>
2025-10-31 16:15:56 +0530

    Collage_APP_frontend

---
+++ .NET/assignments/CollegeApp_View/Controllers/College_App.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp_View.Controllers
+{
+    public class College_App : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ .NET/assignments/CollegeApp_View/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CollegeApp_View.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
